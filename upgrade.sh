d5841ee54b134d3c60e1793bfabfa4cfc195d6e3
arpita patel <arpitap@officebeacon.com>
2018-03-08 16:18:28 +0530

    add ci-cd code

---
+++ upgrade.sh
@@ -0,0 +1,47 @@
+if [ "$TRAVIS_BRANCH" = "master" ]
+then
+    {
+    echo "call $TRAVIS_BRANCH branch"
+    ENV_ID=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects?name=Production" | jq '.data[].id' | tr -d '"'`
+    echo $ENV_ID
+    USERNAME="$DOCKER_USERNAME_FLOWZ";
+    TAG="latest";
+  }
+elif [ "$TRAVIS_BRANCH" = "develop" ]
+then
+    {
+      echo "call $TRAVIS_BRANCH branch"
+      ENV_ID=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects?name=Develop" | jq '.data[].id' | tr -d '"'`
+      echo $ENV_ID
+      USERNAME="$DOCKER_USERNAME";
+      TAG="dev";
+  }
+elif [ "$TRAVIS_BRANCH" = "staging" ]
+then
+    {
+      echo "call $TRAVIS_BRANCH branch"
+      ENV_ID=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects?name=Staging" | jq '.data[].id' | tr -d '"'`
+      echo $ENV_ID
+      USERNAME="$DOCKER_USERNAME";
+      TAG="staging";
+  }
+else
+  {
+      echo "call $TRAVIS_BRANCH branch"
+      ENV_ID=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects?name=QA" | jq '.data[].id' | tr -d '"'`
+      echo $ENV_ID
+      USERNAME="$DOCKER_USERNAME";
+      TAG="qa";
+  }
+fi
+
+SERVICE_ID=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects/$ENV_ID/services?name=rethink-jobqueue-flowz" | jq '.data[].id' | tr -d '"'`
+echo $SERVICE_ID
+
+curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
+-X POST \
+-H 'Accept: application/json' \
+-H 'Content-Type: application/json' \
+-d '{
+     "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:'$USERNAME'/rethink_jobqueue_flowz:'$TAG'","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "machine=cluster-flowz"},"ports": ["4004:4004/tcp"],"environment": {"rdb_host": "'"$RDB_HOST"'","rdb_host": "'"$RDB_PORT"'"},"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 4004,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
+http://rancher.flowz.com:8080/v2-beta/projects/$ENV_ID/services/$SERVICE_ID?action=upgrade
